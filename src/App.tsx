4ad0171e82e89f4a38a41e3e2ec38a9c50038459
afridho <afridho.utama@gmail.com>
2026-08-31 13:27:24 +0700

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ function ChatView() {
 
 function App() {
     return (
-        <ThemeProvider defaultTheme="dark">
+        <ThemeProvider defaultTheme="system">
             <Toaster>
                 <BrowserRouter>
                     <Routes>
